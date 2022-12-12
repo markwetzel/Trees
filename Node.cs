0589e4f61ea21eb4cb8a0e1f63f5c076190c40e8
Mark Wetzel <mark@markwetzel.com>
2022-12-12 17:32:58 -0500

    Initial commit.

---
+++ Node.cs
@@ -0,0 +1,14 @@
+namespace Trees
+{
+    public class Node
+    {
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Trees
+{
+    internal class Program
+    {
+        public static Node BuildTree(Node root)
+        {
+            return null;
+        }
+
+        static void Main()
+        {
+            var n = new Node();
+        }
+    }
+}
